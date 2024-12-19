838522078b237b496c4c34f7eb64c073f63f9b47
Mikhail Ivanov <aka.device@gmail.com>
2024-12-19 18:42:29 +0300

    -Tregulov Spring 01 IoC_DI video 4 Done

---
+++ src/spring_introduction/Cat.java
@@ -0,0 +1,6 @@
+public class Cat implements Pet{
+    @Override
+    public void say() {
+        System.out.println("Meow-Meow");
+    }
+}
---
+++ src/spring_introduction/Dog.java
@@ -0,0 +1,10 @@
+
+
+public class Dog implements Pet {
+    @Override
+    public void say()
+    {
+        System.out.println("Bow-Wow");
+
+    }
+}
