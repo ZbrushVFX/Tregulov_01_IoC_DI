9b6eaf4f61b45bc9a6f917bbe3a80f642489e0f0
Mikhail Ivanov <aka.device@gmail.com>
2024-12-29 04:39:01 +0300

    -Tregulov Spring 01 IoC_DI video 13 Done

--- src/spring_introduction/Dog.java
+++ src/spring_introduction/Dog.java
@@ -1,7 +1,7 @@
 package spring_introduction;
 
 public class Dog implements Pet {
-    private String name;
+   // private String name;
     public Dog(){
         System.out.println("Dog bean is created");
     }
@@ -11,11 +11,19 @@ public class Dog implements Pet {
         System.out.println("Bow-Wow");
     }
 
-    public String getName() {
-        return name;
+    protected void init(){
+        System.out.println("Class Dog: init method");
     }
 
-    public void setName(String name) {
-        this.name = name;
+    private void destroy(){
+        System.out.println("Class Dog: destroy method");
     }
+
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 }
--- src/spring_introduction/Test4.java
+++ src/spring_introduction/Test4.java
@@ -7,13 +7,13 @@ public class Test4 {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationContext2.xml");
 
-        Dog myDog = context.getBean("myPet", Dog.class);
-        myDog.setName("Belka");
-        Dog yourDog = context.getBean("myPet", Dog.class);
-        yourDog.setName("Strelka");
-
-        System.out.println(myDog.getName());
-        System.out.println(yourDog.getName());
+//        Dog myDog = context.getBean("myPet", Dog.class);
+//        myDog.setName("Belka");
+//        Dog yourDog = context.getBean("myPet", Dog.class);
+//        yourDog.setName("Strelka");
+//
+//        System.out.println(myDog.getName());
+//        System.out.println(yourDog.getName());
 
 //        System.out.println("Переменные ссылаются на один и тот же объект? " + (myDog == yourDog));
 //        System.out.println(myDog);
