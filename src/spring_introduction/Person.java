931b2de47893e778cf8857eb3f3fa84b60399d5d
Mikhail Ivanov <aka.device@gmail.com>
2024-12-29 17:51:35 +0300

    -Tregulov Spring 01 IoC_DI video 16 Done

--- src/spring_introduction/Person.java
+++ src/spring_introduction/Person.java
@@ -9,19 +9,19 @@ public class Person {
     private String surname;
     private int age;
 
-    @Autowired
-    public Person(Pet pet) {
-        System.out.println("Person bean is created");
-        this.pet = pet;
-    }
-
-//    public Person() {
+//    @Autowired
+//    public Person(Pet pet) {
 //        System.out.println("Person bean is created");
+//        this.pet = pet;
 //    }
 
-    // pet -> setPet
-    public void setPet(Pet pet) {
-        System.out.println("Class Person set pet");
+    public Person() {
+        System.out.println("Person bean is created");
+    }
+
+    @Autowired
+    public void anyMethodName(Pet pet) {
+        System.out.println("Class Person: anyMethodName");
         this.pet = pet;
     }
 
