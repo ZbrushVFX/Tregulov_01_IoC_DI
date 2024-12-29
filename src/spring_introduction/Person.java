7b18ebe2aec0edfca3a0ceec9e83286b88fd6db1
Mikhail Ivanov <aka.device@gmail.com>
2024-12-29 19:05:32 +0300

    -Tregulov Spring 01 IoC_DI video 18 Done

--- src/spring_introduction/Person.java
+++ src/spring_introduction/Person.java
@@ -1,26 +1,29 @@
 package spring_introduction;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("personBean")
 public class Person {
-    @Autowired
+//    @Autowired
+//    @Qualifier("catBean")
     private Pet pet;
     private String surname;
     private int age;
 
-//    @Autowired
-//    public Person(Pet pet) {
-//        System.out.println("Person bean is created");
-//        this.pet = pet;
-//    }
-
-    public Person() {
+    @Autowired
+    public Person(@Qualifier("catBean") Pet pet) {
         System.out.println("Person bean is created");
+        this.pet = pet;
     }
 
-    //@Autowired
+//    public Person() {
+//        System.out.println("Person bean is created");
+//    }
+
+//    @Autowired
+//    @Qualifier("dog")
     public void setPet(Pet pet) {
         System.out.println("Class Person: set Pet");
         this.pet = pet;
